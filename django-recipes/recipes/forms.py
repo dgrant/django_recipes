bcee686fbd8ee89aa42e8a09330ce21cc3958aeb
davidgrant <davidgrant@fdc508d5-ba53-0410-8be6-4b55b2d6af1c>
2008-11-24 09:15:44 +0000

    moved entire project into sub-directory

---
+++ django-recipes/recipes/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from recipes.models import Recipe
+
+TOPIC_CHOICES = (
+    ('general', 'General enquiry'),
+    ('bug', 'Bug report'),
+    ('suggestion', 'Suggestion'),
+)
+
+# A form for adding/editing a recipe
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+
+class ContactForm(forms.Form):
+    topic = forms.ChoiceField(choices=TOPIC_CHOICES)
+    message = forms.CharField(widget=forms.Textarea(), initial="Replace with your feedback")
+    sender = forms.EmailField(required=False)
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message', '')
+        num_words = len(message.split())
+        if num_words < 4:
+            raise forms.ValidationError("Not enough words!")
+        return message
---
+++ django-recipes/recipes/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponseRedirect
+from django.db.models import Q
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.mail import send_mail
+from models import Recipe
+from forms import ContactForm, RecipeForm
+
+def recipe_add(request):
+    """
+    View for adding a recipe. Could be used to modify recipe too
+    TODO: -add place to add ingredients and directions
+    """
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/recipes/add/thanks/')
+    else:
+        form = RecipeForm()
+    return render_to_response('recipes/recipe_add.html',
+                              {'form': form},
+                              context_instance=RequestContext(request))
+
+def search(request):
+    query = request.GET.get('q', '')
+    if query:
+        qset = (
+            Q(title__icontains=query) |
+            Q(summary__icontains=query)
+        )
+        results = Recipe.objects.filter(qset).distinct()
+    else:
+        results = []
+    return render_to_response("recipes/search.html", {
+        "results": results,
+        "query": query
+    })
+
+def contact(request):
+    """
+    """
+    # If user just submitted
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            #Process form data
+            topic = form.cleaned_data['topic']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data.get('sender', 'noreply@example.com')
+            send_mail(
+                'Feeback from your site, topic: %s' % topic,
+                message,
+                sender,
+                ['davidgrant@gmail.com']
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+
+    # Else if user just loaded page
+    else:
+        form = ContactForm(initial={'sender': 'user@example.com'})
+
+    return render_to_response('recipes/contact.html',
+                              {'form': form},
+                              context_instance=RequestContext(request))
