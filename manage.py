f4b04af530bde4b45fd4a77643f82ff284602661
davidgrant <davidgrant@fdc508d5-ba53-0410-8be6-4b55b2d6af1c>
2009-01-12 08:55:01 +0000

    misc changes

--- manage.py
+++ manage.py
@@ -7,15 +7,15 @@ curr_dir = os.path.abspath(os.path.dirname(__file__))
 for path in [path for path in os.listdir(os.path.join(curr_dir, 'externals')) if path != '.svn']:
     sys.path.append(os.path.join(curr_dir, 'externals', path))
 #Add the django-recipes project
-sys.path.append(os.path.join(curr_dir, 'django-recipes'))
+sys.path.insert(0,os.path.join(curr_dir, 'django-recipes'))
 
 from django.core.management import execute_manager
 try:
-    import settings_simple # Assumed to be in the same directory.
+    import settings # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
 if __name__ == "__main__":
-    execute_manager(settings_simple)
+    execute_manager(settings)
