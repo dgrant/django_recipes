f8531b2b5f5ee5200ba144b0887399572d4e2dd4
davidgrant <davidgrant@fdc508d5-ba53-0410-8be6-4b55b2d6af1c>
2009-06-06 22:17:47 +0000

    removed old django-recipes directoroy

--- django-recipes/README.txt
+++
@@ -1,3 +0,0 @@
-for debian, you need these other packages:
-python-pysqlite2
-python-imaging
--- setpath.py
+++ setpath.py
@@ -2,8 +2,7 @@ import os,sys
 curr_dir = os.path.abspath(os.path.dirname(__file__))
 
 #Add all external dependencies
-for path in [path for path in os.listdir(os.path.join(curr_dir, 'externals'))
-    if path != '.svn']:
-        sys.path.insert(0,os.path.join(curr_dir, 'externals', path))
+for path in [path for path in os.listdir(os.path.join(curr_dir, 'externals')) if path != '.svn']:
+    sys.path.insert(0,os.path.join(curr_dir, 'externals', path))
 #Add the django-recipes project
-#sys.path.insert(0, os.path.join(curr_dir, 'django-recipes'))
+sys.path.insert(0, curr_dir)
