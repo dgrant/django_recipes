c371634893f2bf73986e4a3ae2ba521ca058fc8c
davidgrant <davidgrant@fdc508d5-ba53-0410-8be6-4b55b2d6af1c>
2010-07-03 05:55:28 +0000

    removed externals

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,43 @@
 from fabric.api import *
 from fabric.context_managers import cd
 
+svn_url = 'http://django-recipes.googlecode.com/svn/trunk'
+
 def local():
     env.hosts = ['david@localhost']
-    env.root_dir = '~/svn/django-recipes-pip'
+    env.root_dir = '~/svn/django-recipes-pip-deploy'
 
-def slice():
+def prod():
     env.hosts = ['david@slice:55555']
-    env.root_dir = ''
+    env.root_dir = 'django-recipes-deploy'
     env.user = "david"
     env.key_filename = ["/home/david/.ssh/id_dsa"]
     env.show = ['debug']
 
+def __prereqcheck():
+    require('hosts', provided_by=[local,slice])
+    require('root_dir', provided_by=[local,slice])
+
+def clean():
+    sudo('rm -rf %(root_dir)s' % env)
+
 def setup():
+    __prereqcheck()
     sudo('apt-get update')
-    sudo('apt-get install python-setuptools')
+    sudo('apt-get install python-setuptools subversion')
     sudo('easy_install -U virtualenv')
     sudo('easy_install -U pip')
 
 def virtualenv():
+    __prereqcheck()
+    run('mkdir -p %(root_dir)s' % env)
     with cd('%(root_dir)s' % env):
         run('rm -rf env')
         run('mkdir --parents ~/.pipcache')
         run('virtualenv --no-site-packages env')
-        run('rm -f distribute*.tar.gz')
-        run('pip -E env install -r requirements.txt --download-cache=~/.pipcache')
+
+def deploy():
+    __prereqcheck()
+    with cd('%(root_dir)s' % env):
+        run('svn export ' + (svn_url) + ' src' % env)
+        run('pip -E env install -r src/requirements.txt --download-cache=~/.pipcache')
