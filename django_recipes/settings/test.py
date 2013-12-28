dfbcc97ce4c7cd1a802b476b9ddf2ebce4f8fa5d
David Grant <davidgrant@gmail.com>
2013-12-28 01:18:51 -0800

    add test settings

---
+++ django_recipes/settings/test.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DATABASES = {
+          "default": {
+                    "ENGINE": "django.db.backends.sqlite3",
+                     "NAME": ":memory:",
+                    "USER": "",
+                    "PASSWORD": "",
+                    "HOST": "",
+                    "PORT": "",
+                    },
+}
+
