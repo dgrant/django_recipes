8dd7f7e4c7eb03821fc961758f973c9a3e7f7397
David Grant <davidgrant@gmail.com>
2014-02-12 01:43:36 -0800

    minor

--- backupLocalDB.sh
+++ backupLocalDB.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 mkdir -p backups
-mysqldump -u$1 -p$1 $1 > backups/$1_localhost_`eval date +%Y%m%d_%H%M%S`.sql
+NAME=backups/$1_localhost_`eval date +%Y%m%d_%H%M%S`.sql
+mysqldump -u$1 -p$1 $1 > $NAME
+gzip $NAME
