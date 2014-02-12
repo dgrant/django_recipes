3730f81cd844a7374ea3f172f3f3a77732dc3728
David Grant <davidgrant@gmail.com>
2014-02-12 14:41:43 -0800

    adding fabric file

---
+++ fabfile.py
@@ -0,0 +1,36 @@
+from fabric.api import run, env, cd, shell_env, execute
+
+env.hosts = ['slice:55555']
+ROOT='/home/david/public_html/django/django_recipes/public'
+
+def restart():
+    with cd(ROOT):
+        run('touch ../../django_recipes.ini')
+
+def update():
+    with cd(ROOT):
+        run('git pull')
+
+def schema():
+    with cd(ROOT), shell_env(DJANGO_SETTINGS_MODULE='django_recipes.settings.production'):
+        run('env/bin/python ./manage.py migrate recipes')
+
+def backupdb():
+    with cd(ROOT):
+        run('./backupLocalDB.sh recipes_django')
+
+def static():
+    with cd(ROOT):
+        run('env/bin/python ./manage.py collectstatic --settings=django_recipes.settings.production --noinput --link --clear')
+
+def deploy():
+    execute(update)
+    execute(env)
+    execute(backupdb)
+    execute(schema)
+    execute(static)
+    execute(restart)
+
+def env():
+    with cd(ROOT):
+        run('./createVirtualEnv.sh')
