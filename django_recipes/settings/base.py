5588cc4375800be6bb312151441f123a03efcc4b
David Grant <davidgrant@gmail.com>
2015-12-04 15:22:12 -0800

    remove SECRET_KEY and disable debug mode

--- django_recipes/settings/base.py
+++ django_recipes/settings/base.py
@@ -17,13 +17,10 @@ root = lambda *x: join(abspath(PROJECT_ROOT), *x)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!nz#@kdnz7vt61&+-=bi^r-0_l^m$=6c&lc1^h_-1w6*mw*blu'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
