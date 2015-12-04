d684e5bd15dbd12dd67b3ff4607b0d8317032a29
David Grant <davidgrant@gmail.com>
2015-12-04 15:27:25 -0800

    minor updates

--- backupLocalDB.sh
+++ backupLocalDB.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ $# -eq 0 ]
+then
+  echo "Missing database arg"
+  exit 1
+fi
 mkdir -p backups
 NAME=backups/$1_localhost_`eval date +%Y%m%d_%H%M%S`.sql
 mysqldump -u$1 -p$1 $1 > $NAME
