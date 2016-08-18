f59df7638c2b03f27170547444ba38256f1d4b3e
David Grant <davidgrant@gmail.com>
2016-08-18 01:44:02 -0700

    updated script

--- createVirtualEnv.sh
+++ createVirtualEnv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf env
 virtualenv -p python2 --distribute --no-site-packages env
-env/bin/pip install --download-cache=~/.pip-cache -r requirements.txt
+env/bin/pip install -r requirements.txt
