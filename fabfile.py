f65f283d0f91c1251ad171cb87ee8b5e52f460aa
David Grant <davidgrant@gmail.com>
2019-10-21 13:02:20 -0700

    Update fabfile with some fixes

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,5 @@
+import re
+
 from fabric.api import run, env, cd, shell_env, execute, sudo
 from fabric.network import ssh
 
@@ -41,10 +43,25 @@ def deploy():
     execute(static)
     execute(restart)
 
+ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
+
 def env():
     with cd(ROOT):
+        run('pipenv --rm')
+        run('pipenv --python 3.7')
         run('pipenv install')
-        pipenv_env_dir = run('pipenv --venv')
+
+        pipenv_env_dir = run('pipenv --bare --venv')
+        print("pipenv_env_dir=",pipenv_env_dir)
+
+        pipenv_env_dir = ansi_escape.sub("", pipenv_env_dir)
+        print("pipenv_env_dir=",pipenv_env_dir)
+
+        pipenv_env_dir = pipenv_env_dir.strip()
+        print("pipenv_env_dir=",pipenv_env_dir)
+
         pipenv_env_dir = pipenv_env_dir.replace('/', '\\/')
+        print("pipenv_env_dir=",pipenv_env_dir)
+
         sudo("sed -e 's/home=.*/home={0}/g' -i.bak {1}".format(pipenv_env_dir, INI_FILE))
         run("cat " + INI_FILE)
